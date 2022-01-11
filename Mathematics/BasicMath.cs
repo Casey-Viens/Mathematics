a246f0709deaec145cf0be408167b63a7f63c60a
Casey-Viens <casey.viens@gmail.com>
2022-01-11 18:42:13 -0500

    In class first push

---
+++ Mathematics/BasicMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    public class BasicMath
+    {
+        /// <summary>
+        /// This is a function that adds two numbers together
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public double AddNumbers(double num1, double num2)
+        {
+            // This adds two numbers
+            return num1 + num2;
+        }
+
+        public double SubtractNumbers(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double MultiplyNumbers(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double DivideNumbers(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
