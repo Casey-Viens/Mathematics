cb496dfaa4831ab703e61af44f705113dcdb34d6
Casey-Viens <casey.viens@gmail.com>
2022-01-18 18:49:37 -0500

    Push from week2 class

---
+++ Week1/MathematicsConsole/Program.cs
@@ -0,0 +1,66 @@
+﻿using Mathematics;
+using System;
+
+namespace MathematicsConsole
+{
+    class Program
+    {
+        private static double _num1;
+        private static double _num2;
+        private static string _operand;
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            //foreach (var a in args)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //Console.ReadLine();
+            var math = new BasicMath();
+            AreArgumentsValid(args);
+            switch (_operand)
+            {
+                case "add":
+                    Console.WriteLine($"{_num1} + {_num2} = {math.AddNumbers(_num1, _num2)}");
+                    break;
+                case "sub":
+                    Console.WriteLine($"{_num1} - {_num2} = {math.SubtractNumbers(_num1, _num2)}");
+                    break;
+                case "mul":
+                    Console.WriteLine($"{_num1} * {_num2} = {math.MultiplyNumbers(_num1, _num2)}");
+                    break;
+                case "div":
+                    Console.WriteLine($"{_num1} / {_num2} = {math.DivideNumbers(_num1, _num2)}");
+                    break;
+                default:
+                    Console.WriteLine("The Operand you chose is not a valid option. Please Enter Add, Sub, Mul, or Div");
+                    break;
+            }
+            
+        }
+
+        private static void AreArgumentsValid(string[] args)
+        {
+            _operand = args[1].ToLower();
+            _num1 = Numparser(args[2]);
+            _num2 = Numparser(args[3]);
+            Console.WriteLine("All arguments are valid");
+            Console.ReadLine();
+        }
+
+        public static double Numparser(string arg)
+        {
+            double num;
+            if (Double.TryParse(arg, out num))
+            {
+                return num;
+            }
+            else
+            {
+                Console.WriteLine($"Unable to parse {num}");
+                Environment.Exit(99);
+            }
+            return 0;
+        }
+    }
+}
