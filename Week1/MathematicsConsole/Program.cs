22e771381620bea6526861650935d6834509783b
Casey Viens <ceviens@email.neit.edu>
2022-01-25 06:58:19 -0500

    Week 2 Console Application for AdvMath Homework

--- Week1/MathematicsConsole/Program.cs
+++ Week1/MathematicsConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Mathematics;
 using System;
+using System.Collections.Generic;
 
 namespace MathematicsConsole
 {
@@ -7,6 +8,7 @@ namespace MathematicsConsole
     {
         private static double _num1;
         private static double _num2;
+        private static List<double> _nums = new List<double>();
         private static string _operand;
         static void Main()
         {
@@ -17,6 +19,7 @@ namespace MathematicsConsole
             //}
             //Console.ReadLine();
             var math = new BasicMath();
+            var advMath = new AdvMath();
             AreArgumentsValid(args);
             switch (_operand)
             {
@@ -32,6 +35,18 @@ namespace MathematicsConsole
                 case "div":
                     Console.WriteLine($"{_num1} / {_num2} = {math.DivideNumbers(_num1, _num2)}");
                     break;
+                case "area":
+                    Console.WriteLine($"{_num1} * {_num2} = {advMath.CalcArea(_num1, _num2)}");
+                    break;
+                case "average":
+                    Console.WriteLine($"The average is = {advMath.CalcAvg(_nums)}");
+                    break;
+                case "square":
+                    Console.WriteLine($"{_num1} ^ 2 = {advMath.SquareNumber(_num1)}");
+                    break;
+                case "pythagorean":
+                    Console.WriteLine($"C = {advMath.PythagoreanTheorem(_num1, _num2)}");
+                    break;
                 default:
                     Console.WriteLine("The Operand you chose is not a valid option. Please Enter Add, Sub, Mul, or Div");
                     break;
@@ -42,8 +57,25 @@ namespace MathematicsConsole
         private static void AreArgumentsValid(string[] args)
         {
             _operand = args[1].ToLower();
-            _num1 = Numparser(args[2]);
-            _num2 = Numparser(args[3]);
+            if(_operand == "square")
+            {
+                _num1 = Numparser(args[2]);
+            }else if (_operand == "average")
+            {
+                for(int i = 2; i < args.Length; i++)
+                {
+                    _nums.Add(Numparser(args[i]));
+                }
+            }else if(args.Length == 4)
+            {
+                _num1 = Numparser(args[2]);
+                _num2 = Numparser(args[3]);
+            }
+            else
+            {
+                Console.WriteLine($"Operation could not be complete");
+                Environment.Exit(99);
+            }
             Console.WriteLine("All arguments are valid");
             Console.ReadLine();
         }
